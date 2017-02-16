81d72d80f1169f1c0e7c3232a751d1ccf98db6ee
Chh-Huang <chua5342@uni.sydney.edu.au>
2017-02-16 14:01:18 +1100

    Add files via upload

--- graphDrawing.m
+++ graphDrawing.m
@@ -40,7 +40,6 @@ disp ('distanceSet2');
 %disp (distanceSet2);
 disp (max(distanceSet2));
 disp (min(distanceSet2));
-%hist(distanceSet2,10);
 [n2, x2] = hist(distanceSet2,10,'r');
 
 figure;
@@ -57,13 +56,26 @@ for m = 1:3540
     end
 end
 disp ('distanceSet3');
+%disp (distanceSet3);
 disp (max(distanceSet3));
 disp (min(distanceSet3));
 [n3, x3] = hist(distanceSet3,10,'r');
 
 close all
+
+%{
+%Bar Chart
 bar(x1,n1,'r')
 hold on;
+%[n2,x2] = hist(distanceSet2);
 bar(x2,n2,'b')
-hold on;
+%[n3,x3] = hist(distanceSet3);
 bar(x3,n3,'g')
+hold off
+%}
+
+%curve
+values1=spcrv([[x1(1) x1 x1(end)];[n1(1) n1 n1(end)]],3,1000);
+values2=spcrv([[x2(1) x2 x2(end)];[n2(1) n2 n2(end)]],3,1000);
+values3=spcrv([[x3(1) x3 x3(end)];[n3(1) n3 n3(end)]],3,1000);
+plot(values1(1,:),values1(2,:),'r',values2(1,:),values2(2,:),'b',values3(1,:),values3(2,:),'g')
